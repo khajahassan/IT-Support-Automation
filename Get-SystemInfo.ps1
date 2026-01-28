d2d42fc049ce5acc17a012f848ad8bbd6baa6eb1
khajahassan <khaja.hassan@gmail.com>
2026-01-28 17:02:37 -0500

    Update Get-SystemInfo.ps1

--- Get-SystemInfo.ps1
+++ Get-SystemInfo.ps1
@@ -1,6 +1,10 @@
 # Get-SystemInfo.ps1
-# This script gets basic system info for troubleshooting.
-Write-Host "Gathering System Information..." -ForegroundColor Cyan
-Get-ComputerInfo | Select-Object WindowsProductName, CsProcessors, OsArchitecture
-Get-NetIPAddress -AddressFamily IPv4 | Select-Object InterfaceAlias, IPAddress
+# This script prints basic system info for troubleshooting.
+Write-Host "--- SYSTEM INFO ---" -ForegroundColor Cyan
+Get-ComputerInfo | Select-Object WindowsProductName, CsProcessors, OsArchitecture | Out-Host
+Write-Host "--- NETWORK INFO ---" -ForegroundColor Yellow
+Get-NetIPAddress -AddressFamily IPv4 | Where-Object PrefixOrigin -ne "WellKnown" | Select-Object InterfaceAlias, IPAddress | Out-Host
+Write-Host "--- DISK SPACE ---" -ForegroundColor Green
+Get-Volume -DriveLetter C | Select-Object DriveLetter, FileSystemLabel, @{Name="Free(GB)";Expression={[math]::Round($_.SizeRemaining / 1GB, 2)}} | Out-Host
 Write-Host "Audit Complete." -ForegroundColor Green
+
